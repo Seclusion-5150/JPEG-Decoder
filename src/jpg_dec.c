e7eaf6458d1659003901cf0b535d5dd62f257354
Seclusion <yamuchan@protonmail.com>
2021-04-10 16:04:48 -0400

    Properly parsed the Exif/TIFF header and added a new function(WIP) to parse the next marker

--- src/jpg_dec.c
+++ src/jpg_dec.c
@@ -8,80 +8,106 @@ void print_matrix(byte  **matrix, ulong len)
 		for(j = 0; j < 2; j++)
 		{
 			printf("[%d][%d]: %#1x\n",i, j, (unsigned int)matrix[i][j]);
-			//getchar();
 		}
 		printf("\n");
 	}
 }
-void print_array(byte *array, int len)
+void print_array(byte *array, int len, char *type)
 {
 	int i;
-	for(i = 0; i < len;i++)
+
+	if(!strcmp(type, "Character")) // if strcmp is equal to 0 then it is true. Note: 0 in C is FALSE which is why I use the '!' symbol
 	{
-		printf("%c",array[i]);
+		for(i = 0; i < len;i++)
+		{
+			printf("%c",array[i]);
+		}
+	}
+	else if(!strcmp(type, "Integer"))
+	{
+		for(i = 0; i < len; i++)
+		{
+			printf("%d", array[i]);
+		}
+
+	}
+	else if (!strcmp(type, "Hexadecimal"))
+	{
+		for(i = 0; i < len; i++)
+		{
+			printf("%#1x ", array[i]);
+		}
 	}
 	printf("\n");
 }
-void parse_appn(struct file_data *data)
+void parse_tiff_header(struct file_data *data)
 {
+	if(exif.header.ifd0_ptr == 8)// if the header has already been filled set isFilled to true and return to the main function
+	{
+		exif.header.ifd0_ptr = TRUE;
+		return;
+	}
 	byte c = 1;
-	//byte string[4096];
-	//fscanf(data->image, "%c", &c);
-	
+
 	/*
-	Exif File format
+	Exif(TIFF) Header File format
+	Note: Some of the bytes contained in each section are zeros and in this implementation i skip over storing the zeros. For example instead of the Identifier being 6 bytes I have it store 5 bytes
 	TODO: first  the Identifier [Not stored] 6 bytes
 	second  the Endianness			 2 bytes
 	third   the Signature			 2 bytes A fixed value
-	fourth  the IFD0Pointer			 4 bytes A offset of the 0th IFD [Usually 8 not stored]
-	fifth   IFD0				 ~ bytes Main image IFD
-	sixth	IFD0@SubIFD			 ~ bytes Exif private tags [Optional, linked by IFD0]
-	seventh IFD0@SubIFD@Interop		 ~ bytes Interoperability IFD [Optional, linked by SubIFD]
-	eight	IFD0@GPS			 ~ bytes GPS IFD [Optional, linked by IFD0]
-	ninth   APP1@IFD1			 ~ bytes Thumbnail IFD[Optional, pointed to by IFD0]
-	tenth	ThumbnailData			 ~ bytes Thumbnail image [Optional, 0xffd8(SOI) ... 0xffd9(EOI)]
+	fourth  the IFD0Pointer			 4 bytes A offset of the 0th IFD [Usually 8 not stored] // points to the beginning of the Header which starts at the address of the Endianness
 	*/
-	/*
-	Exif XMP format(Adobe Photoshop)
-	TODO: I need to research this more
-	*/
-	int i = 0, j = 0, k = 0;
-	while(c!=' ')
+
+	int i = 0, j = 0;
+
+	while(c != 0xFF)
 	{
 
 		fscanf(data->image, "%c", &c);
 
-		if(c!=0) // temporary if statements until I figure out how to properly parse the Exif header but it should work for now
+		if(c != 0) // it seems that the Exif format uses 0s for padding between information so I will filter them out
 		{
-
 			if ( i < 5)
 			{
-				exif.identifier[i++] = c;
+				exif.header.identifier[i++] = c;
 			}
-			else if (( i >= 5 ) && ( i < 8 ))
+			else if (( i >=5 ) && ( i < 7 ))
 			{
 
-				exif.endianness[j++] = c;
+				exif.header.endianness[j++] = c;
+				i++;
+			}
+			else if (( i >= 7 ) && ( i < 8))
+			{
+				exif.header.signature = c;
+				i++;
+			}
+			else if (i == 8)
+			{
+				exif.header.ifd0_ptr = c;
 				i++;
 			}
 		}
-		if (( i >= 8 ) && ( i < 11))
-		{
-				exif.signature[k++] = c;
-				i++;
-		}
-
 	}
+
 	printf("Identifier: ");
-	print_array(exif.identifier, 6);
+	print_array(exif.header.identifier, 5, "Character");
 	printf("Endianness: ");
-	print_array(exif.endianness, 2);
-	printf("Signature: ");
-	print_array(exif.signature,  2);
+	print_array(exif.header.endianness, 2, "Character");
+	printf("Signature: %d\n", exif.header.signature);
+	printf("IFD0 Pointer: %d\n", exif.header.ifd0_ptr);
+
+	fseek(data->image, -1L, SEEK_CUR);// Goes back one so that when it returns to the main function it will be able to parse the next marker
+}
+
+void parse_app1_subifd(struct file_data * data)
+{
+
 }
 int main(int argc, char *argv[])
 {
-	system("cd ..");
+	//system("cd ..");
+	exif.header.isFilled = FALSE;
 	struct file_data data;
 	data.image = fopen("./res/mouse.jpg", "rb"); // put the path to your image here. 
 	data.isOver = FALSE;
@@ -103,34 +129,37 @@ int main(int argc, char *argv[])
 						printf("Start of Image: %#1x\n", (uint)c);
 						break;
 					case EOI:
-						printf("End of Image: %#1x\n", (uint)c);
+						printf("\nEnd of Image: %#1x\n", (uint)c);
 						data.isOver = TRUE;
 						break;
 					case APP0:
-						printf("This is a JFIF file: %#1x\n", (uint)c);
-						parse_appn(&data);
+						printf("\nThis is a JFIF file: %#1x\n", (uint)c);
 						break;
 					case APP1:
-						printf("This is an EXIF metadata Structure: %#1x\n",(uint)c);
-						parse_appn(&data);
+						printf("\nThis is an EXIF metadata Structure: %#1x\n",(uint)c);
+						if(!exif.header.isFilled) parse_tiff_header(&data);
+						else parse_app1_subifd(&data);
+						break;
+					case APP12:
+						printf("\nThis is the 0th IFD\n");
 						break;
 					case SOF0:
-						printf("Start of frame(baseline DCT): %#1x\n", (uint)c);
+						printf("\nStart of frame(baseline DCT): %#1x\n", (uint)c);
 						break;
 					case SOF2:
-						printf("Start of frame(progressive DCT): %#1x\n", (uint)c);
+						printf("\nStart of frame(progressive DCT): %#1x\n", (uint)c);
 						break;
 					case DHT:
-						printf("This specifies one or more Huffman tables: %#1x\n", (uint)c);
+						printf("\nThis specifies one or more Huffman tables: %#1x\n", (uint)c);
 						break;
 					case DQT:
-						printf("This specifies one or more quantization tables: %#1x\n", (uint)c);
+						printf("\nThis specifies one or more quantization tables: %#1x\n", (uint)c);
 						break;
 					case DRI:
-						printf("This specifies the interval between RSTn markers in MCUs: %#1x\n", (uint)c);
+						printf("\nThis specifies the interval between RSTn markers in MCUs: %#1x\n", (uint)c);
 						break;
 					case SOS:
-						printf("Start of Scan: %#1x\n", (uint)c);
+						printf("\nStart of Scan: %#1x\n", (uint)c);
 						break;
 					case RST0:
 						break;
--- src/jpg_dec.h
+++ src/jpg_dec.h
@@ -8,39 +8,49 @@
 
 // Format 0xFFXX [YYYY ...]
 
-#define SOI  0xd8 // start of image
-#define EOI  0xd9 // end of image
+#define SOI  	0xd8 // start of image
+#define EOI  	0xd9 // end of image
 
-#define SOF0 0xC0 //variable size
-#define SOF2 0xC2 //start of frame: indicates that this is a baseline DCT-Based JPEG, and specifies the width, height, number of components
+#define SOF0	0xC0 //variable size
+#define SOF2	0xC2 //start of frame: indicates that this is a baseline DCT-Based JPEG, and specifies the width, height, number of components
 
-#define DHT  0xC4 //specifies one or more huffman tables
-#define DQT  0xDB //specifies one or more quantization tables
-#define DRI  0xDD //specifies the interval between RSTn markers, in Minimum coded Units(MCUs)
+#define DHT  	0xC4 //specifies one or more huffman tables
+#define DQT  	0xDB //specifies one or more quantization tables
+#define DRI  	0xDD //specifies the interval between RSTn markers, in Minimum coded Units(MCUs)
 
-#define SOS  0xDA // start of scan
+#define SOS  	0xDA // start of scan
 //Restart markers the low bits values being from 0 to 7
-#define RST0 0xD0
-#define RST1 0xD1
-#define RST2 0xD2
-#define RST3 0xD3
-#define RST4 0xD4
-#define RST5 0xD5
-#define RST6 0xD6
-#define RST7 0xD7
+#define RST0 	0xD0
+#define RST1 	0xD1
+#define RST2 	0xD2
+#define RST3 	0xD3
+#define RST4 	0xD4
+#define RST5 	0xD5
+#define RST6 	0xD6
+#define RST7 	0xD7
 //Application Specific markers
-#define APP0 0xE0 //JFIF marker
-#define APP1 0xE1 //EXIF marker
-#define APP2 0xE2
-#define APP3 0xE3
+#define APP0 	0xE0 //JFIF marker
+#define APP1 	0xE1 //EXIF marker
+#define APP2 	0xE2
+#define APP3 	0xE3
+#define APP4 	0xE4
+#define APP5 	0xE6
+#define APP6 	0xE7
+#define APP7 	0xE8
+#define APP8 	0xE9
+#define APP9 	0xE10
+#define APP10	0xE11
+#define APP11 	0xE12
+#define APP12 	0xEC
+#define APP13 	0xE14
 
-#define COM  0xFE //contains a comment
+#define COM  	0xFE //contains a comment
 
 typedef int bool;
 typedef unsigned  char byte;
 typedef unsigned  int  uint;
 typedef unsigned  long ulong;
-;
+
 struct pixel_data
 {
 	/*RGB can be computed directly from these values the problem is knowing how ...*/
@@ -62,12 +72,17 @@ struct file_data
 	unsigned long file_len;
 	bool isOver;
 };
-
+struct tiff_header
+{
+	byte identifier[5];
+	byte endianness[2];
+	byte signature;
+	byte ifd0_ptr;
+	bool isFilled;
+};
 struct exif_data
 {
-	byte identifier[29];
-	byte endianness[2];
-	byte signature[4];
+	struct tiff_header header;
 };
 
 struct exif_data exif;
